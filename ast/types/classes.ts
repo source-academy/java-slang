27758ddf9c99306e182c34db04d925956349ff8d
1001mei <77192251+1001mei@users.noreply.github.com>
2023-09-11 21:35:16 +0800

    Fix indentation inconsistencies

--- ast/types/classes.ts
+++ ast/types/classes.ts
@@ -1,56 +1,56 @@
 export type ClassDeclaration = NormalClassDeclaration;
 
 export interface NormalClassDeclaration {
-    classModifier: Array<ClassModifier>;
-    typeIdentifier: Identifier;
-    classBody: Array<ClassBodyDeclaration>;
+  classModifier: Array<ClassModifier>;
+  typeIdentifier: Identifier;
+  classBody: Array<ClassBodyDeclaration>;
 }
 
 export type ClassModifier =
-    | "public"
-    | "protected"
-    | "private"
-    | "abstract"
-    | "static"
-    | "final"
-    | "sealed"
-    | "non-sealed"
-    | "strictfp";
+  | "public"
+  | "protected"
+  | "private"
+  | "abstract"
+  | "static"
+  | "final"
+  | "sealed"
+  | "non-sealed"
+  | "strictfp";
 
 export type ClassBodyDeclaration = ClassMemberDeclaration;
 export type ClassMemberDeclaration = MethodDeclaration;
 
 export interface MethodDeclaration {
-    methodModifier: Array<MethodModifier>;
-    methodHeader: MethodHeader;
-    methodBody: MethodBody;
+  methodModifier: Array<MethodModifier>;
+  methodHeader: MethodHeader;
+  methodBody: MethodBody;
 }
 
 export type MethodModifier =
-    | "public"
-    | "protected"
-    | "private"
-    | "abstract"
-    | "static"
-    | "final"
-    | "synchronized"
-    | "native"
-    | "strictfp";
+  | "public"
+  | "protected"
+  | "private"
+  | "abstract"
+  | "static"
+  | "final"
+  | "synchronized"
+  | "native"
+  | "strictfp";
 
 export interface MethodHeader {
-    result: Result;
-    methodDeclarator: MethodDeclarator;
+  result: Result;
+  methodDeclarator: MethodDeclarator;
 }
 
 export type Result = "void";
 export interface MethodDeclarator {
-    identifier: Identifier;
-    formalParameterList: Array<FormalParameter>;
+  identifier: Identifier;
+  formalParameterList: Array<FormalParameter>;
 }
 
 export interface FormalParameter {
-    unannType: UnannType;
-    variableDeclaratorId: Identifier;
+  unannType: UnannType;
+  variableDeclaratorId: Identifier;
 }
 
 export type UnannType = string;
