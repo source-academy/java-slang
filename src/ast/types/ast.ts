f48524cf51f1642bca70b074dfff462d93d17292
1001mei <77192251+1001mei@users.noreply.github.com>
2023-10-23 23:51:23 +0800

    Update Node type

--- src/ast/types/ast.ts
+++ src/ast/types/ast.ts
@@ -3,19 +3,13 @@ import {
   Block,
   BlockStatement,
   Expression,
-  ExpressionName,
-  Literal,
-  LocalVariableDeclarationStatement,
 } from "./blocks-and-statements";
 
 interface NodeMap {
   CompilationUnit: CompilationUnit;
-  LocalVariableDeclarationStatement: LocalVariableDeclarationStatement;
   Block: Block;
   BlockStatement: BlockStatement;
   Expression: Expression;
-  ExpresssionName: ExpressionName;
-  Literal: Literal;
 }
 
 export type Node = NodeMap[keyof NodeMap];
