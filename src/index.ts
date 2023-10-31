70dd831b8d7b47b885bb853e843b4c7f5a801084
Bryan Loh <bryanlohxz@gmail.com>
2023-10-31 13:14:55 +0800

    Move wrongly edited index.ts into types submodule

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Node } from "./ast/types/ast";
-import { check } from "./types/checker";
-
-export type TypeCheckResult = { hasTypeErrors: boolean };
-
-export const typeCheck = (ast: Node): TypeCheckResult => {
-  const result = check(ast);
-  return { hasTypeErrors: result.errors.length > 0 };
-};
