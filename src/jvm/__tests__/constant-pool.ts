5018abf9e014e895b2b149cccc5666811a008e75
Aprup Kale <aprup.kale@gmail.com>
2024-10-15 13:52:23 +0800

    Add tests for constant-pool.ts

--- src/jvm/__tests__/constant-pool.ts
+++ src/jvm/__tests__/constant-pool.ts
@@ -47,39 +47,39 @@ beforeEach(() => {
     { tag: CONSTANT_TAG.Methodref, classIndex: 9, nameAndTypeIndex: 7 },
     { tag: CONSTANT_TAG.InterfaceMethodref, classIndex: 9, nameAndTypeIndex: 7 },
     { tag: CONSTANT_TAG.MethodHandle, referenceKind: 1, referenceIndex: 12 }
-  ];
+  ]
 
-  constantPool = new ConstantPool(classData, constantInfos);
+  constantPool = new ConstantPool(classData, constantInfos)
 })
 
 describe('ConstantPool', () => {
   test('Should correctly initialize the constant pool with all provided constant types', () => {
-    expect(constantPool.size()).toBe(15);
-    expect(constantPool.get(1)).toBeInstanceOf(ConstantInteger);
-    expect(constantPool.get(2)).toBeInstanceOf(ConstantFloat);
-    expect(constantPool.get(3)).toBeInstanceOf(ConstantLong);
-    expect(constantPool.get(4)).toBeInstanceOf(ConstantDouble);
-    expect(constantPool.get(5)).toBeInstanceOf(ConstantUtf8);
-    expect(constantPool.get(6)).toBeInstanceOf(ConstantString);
-    expect(constantPool.get(7)).toBeInstanceOf(ConstantNameAndType);
-    expect(constantPool.get(8)).toBeInstanceOf(ConstantMethodType);
-    expect(constantPool.get(9)).toBeInstanceOf(ConstantClass);
-    expect(constantPool.get(10)).toBeInstanceOf(ConstantInvokeDynamic);
-    expect(constantPool.get(11)).toBeInstanceOf(ConstantFieldref);
-    expect(constantPool.get(12)).toBeInstanceOf(ConstantMethodref);
-    expect(constantPool.get(13)).toBeInstanceOf(ConstantInterfaceMethodref);
-    expect(constantPool.get(14)).toBeInstanceOf(ConstantMethodHandle);
+    expect(constantPool.size()).toBe(15)
+    expect(constantPool.get(1)).toBeInstanceOf(ConstantInteger)
+    expect(constantPool.get(2)).toBeInstanceOf(ConstantFloat)
+    expect(constantPool.get(3)).toBeInstanceOf(ConstantLong)
+    expect(constantPool.get(4)).toBeInstanceOf(ConstantDouble)
+    expect(constantPool.get(5)).toBeInstanceOf(ConstantUtf8)
+    expect(constantPool.get(6)).toBeInstanceOf(ConstantString)
+    expect(constantPool.get(7)).toBeInstanceOf(ConstantNameAndType)
+    expect(constantPool.get(8)).toBeInstanceOf(ConstantMethodType)
+    expect(constantPool.get(9)).toBeInstanceOf(ConstantClass)
+    expect(constantPool.get(10)).toBeInstanceOf(ConstantInvokeDynamic)
+    expect(constantPool.get(11)).toBeInstanceOf(ConstantFieldref)
+    expect(constantPool.get(12)).toBeInstanceOf(ConstantMethodref)
+    expect(constantPool.get(13)).toBeInstanceOf(ConstantInterfaceMethodref)
+    expect(constantPool.get(14)).toBeInstanceOf(ConstantMethodHandle)
 
-    expect((constantPool.get(1) as ConstantInteger).get()).toBe(42);
-    expect((constantPool.get(2) as ConstantFloat).get()).toBe(3.14);
-    expect((constantPool.get(3) as ConstantLong).get()).toBe(BigInt(1234567890));
-    expect((constantPool.get(4) as ConstantDouble).get()).toBe(2.71828);
-    expect((constantPool.get(5) as ConstantUtf8).get()).toBe('Hello, World!');
-  });
+    expect((constantPool.get(1) as ConstantInteger).get()).toBe(42)
+    expect((constantPool.get(2) as ConstantFloat).get()).toBe(3.14)
+    expect((constantPool.get(3) as ConstantLong).get()).toBe(BigInt(1234567890))
+    expect((constantPool.get(4) as ConstantDouble).get()).toBe(2.71828)
+    expect((constantPool.get(5) as ConstantUtf8).get()).toBe('Hello, World!')
+  })
 
   test('Should throw an error when get method is called with an invalid index', () => {
     const constantPool = new ConstantPool(classData, [
-      { tag: CONSTANT_TAG.Utf8, length: 5, value: 'Test' },
+      { tag: CONSTANT_TAG.Utf8, length: 5, value: 'Test' }
     ])
 
     expect(constantPool.get(-1)).toBe(undefined)
